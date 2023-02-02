feffe5773c8535618b1ce5faf814528d8565db16
Jorge E Osorio G <jorgeeosoriog@Jorges-iMac-9.lan>
2023-02-01 19:13:10 -0500

    Created quote app

---
+++ index.js
@@ -0,0 +1,6 @@
+const position = Math.floor(Math.random()*3)
+const quotes = [
+"I have classes Mondays, Wednesdays, Thursdays, and Fridays",
+"I am enrolled in three different classes this semester",
+"I drive around 25 minutes to get to UCF Downtown Campus"];
+console.log(quotes[position]);
